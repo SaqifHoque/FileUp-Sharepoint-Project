32b23e0a7fb2cd82dda7a1211d9e15280ac05b90
SaqifHaque <saqifhaque@gmail.com>
2023-10-27 00:17:38 +0600

    Upload File request Model added

---
+++ DocUploader/Shared/Dtos/UploadFileRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocUploader.Shared.Dtos
+{
+    public class UploadFileRequestModel
+    {
+        public string SiteId { get; set; }
+        public string DriveId { get; set; }
+        public string FolderId { get; set; }
+        public string FileLocalPath { get; set; }
+        public FileInfo FileInfo { get; set; }
+    }
+}
