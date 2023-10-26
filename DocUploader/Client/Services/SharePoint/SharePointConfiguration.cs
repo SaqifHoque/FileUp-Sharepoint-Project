4efd7b5078550a501deeb4a2bad1178b0e328eda
SaqifHaque <saqifhaque@gmail.com>
2023-10-27 00:20:19 +0600

    Sharepoint Configuration Modal Added

---
+++ DocUploader/Client/Services/SharePoint/SharePointConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace DocUploader.Client.Services.SharePoint
+{
+    public class SharePointConfiguration
+    {
+        public string TenantId { set; get; }
+        public string ApplicationId { set; get; }
+        public string ClientSecret { set; get; }
+        public string SiteId { set; get; }
+        public string DriveId { set; get; }
+    }
+}
