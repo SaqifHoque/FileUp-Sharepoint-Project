ff20e17cda9bfb93e0cd645e9dda29c4dc74f0a2
SaqifHaque <saqifhaque@gmail.com>
2023-10-28 12:46:20 +0600

    Document Category Model added

---
+++ DocUploader/Shared/Models/DocumentCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocUploader.Shared.Models
+{
+    public class DocumentCategory
+    {
+        [Key]
+        public int DocumentCategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public ICollection<ClientDocumentCategory> ClientDocumentCategories { get; set; }
+    }
+}
