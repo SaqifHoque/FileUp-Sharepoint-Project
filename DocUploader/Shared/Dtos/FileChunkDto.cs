a00c55ac1bba38a23fe4bc1b1532437ecc42c19d
SaqifHaque <saqifhaque@gmail.com>
2023-10-28 12:46:01 +0600

    File Chunk Dto Updated

--- DocUploader/Shared/Dtos/FileChunkDto.cs
+++ DocUploader/Shared/Dtos/FileChunkDto.cs
@@ -12,7 +12,7 @@ namespace DocUploader.Shared.Dtos
         public long Offset { get; set; }
         public byte[]? Data { get; set; }
         public bool FirstChunk = false;
-        public bool LastChunk = false;
+        public bool LastChunk { get; set; }
         public string? To { get; set; } = null;
         public int ClientId { get; set; }
         public int RequestId { get; set; }
