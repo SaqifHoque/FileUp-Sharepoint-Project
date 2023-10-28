99377d3bf037565d590788be9d4cc06df2a19e20
SaqifHaque <saqifhaque@gmail.com>
2023-10-28 12:46:32 +0600

    Client Document Category Model added

---
+++ DocUploader/Shared/Models/ClientDocumentCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocUploader.Shared.Models
+{
+    public class ClientDocumentCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public Client Client { get; set; }
+
+        public int DocumentCategoryId { get; set; }
+        public DocumentCategory DocumentCategory { get; set; }
+    }
+}
