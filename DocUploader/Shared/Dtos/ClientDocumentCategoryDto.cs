57e8277d5ecf648d04462f4e3c2a1da27d095ca5
SaqifHaque <saqifhaque@gmail.com>
2023-10-28 12:45:38 +0600

    client categoryDto added

---
+++ DocUploader/Shared/Dtos/ClientDocumentCategoryDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocUploader.Shared.Dtos
+{
+    public class ClientDocumentCategoryDto
+    {
+        public int Id { get; set; }
+        public string DocumentCategoryName {  get; set; }
+        public string ClientName { get; set; }
+    }
+}
